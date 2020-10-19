0fc9bb4beaa09cd6b9b32d15f033efd530b0d0c7
lxk <kasperlxk@outlook.com>
2020-10-19 13:16:18 +0800

    0.2.1

--- mailsender/utility.py
+++ mailsender/utility.py
@@ -1,7 +1,8 @@
 import logging
 
 LOG = logging.getLogger("MS")
-handler = logging.StreamHandler(); handler.setFormatter(logging.Formatter(
-    fmt='[%(asctime)s][MS %(levelname)s] %(message)s',
-    datefmt='%b %d %I:%M',
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(
+    fmt     = '[%(asctime)s][MS %(levelname)s] %(message)s',
+    datefmt = '%m-%d %H:%M',
 )) ; LOG.addHandler(handler)
