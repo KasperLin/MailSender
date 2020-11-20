0ec7e78007c9e99375f5a011503ddbab8095fb22
lxk <kasperlxk@outlook.com>
2020-11-20 17:34:46 +0800

    0.2.4.2

--- mailsender/send.py
+++ mailsender/send.py
@@ -8,8 +8,6 @@ from mailsender.utility import LOG
 from mailsender.init    import MailSender
 from mailsender.html    import HTML_HEAD, HTML_TAIL
 
-ENCODING = "utf-8"
-
 
 class MailSender(MailSender):
     ''' A Friendly Python E-mail Sending Tool '''
@@ -21,12 +19,12 @@ class MailSender(MailSender):
 		content_type :str  = "html",
 	) -> bool:
         receivers:list = _get_receivers(self.sender, to)
-        message:str = _get_message(
-            title   = header, 
-            from    = self.sender, 
-            to      = receivers, 
-            dtype   = content_type, 
-            content = content, 
+        message  :str  = _get_message(
+            title   = header,
+            sender  = self.sender,
+            to      = receivers,
+            dtype   = content_type,
+            content = content,
         )
         self.server.sendmail(self.sender, receivers, message)
         LOG.info(f"Mail ({header}) sent to {len(receivers)} users.")
@@ -50,15 +48,22 @@ def _get_receivers(sender, to:list) -> list:
 	return receivers
 
 
-def _get_message(title:str, from:str, to:list, dtype:str, content:str) -> str:
+def _get_message(
+    title  :str,
+    sender :str,
+    to     :list,
+    dtype  :str,
+    content:str,
+) -> str:
     ''' Get E-mail Multipart Message '''
+    ENCODING = "utf-8"
+
     message:MIMEMultipart = MIMEMultipart()
+    message["Subject"]    = Header(title, ENCODING).encode()
+    message["From"]       = sender
+    message["To"]         = ','.join(to)
 
-    message["Subject"] = Header(title, ENCODING).encode()
-    message["From"]    = from
-    message["To"]      = ','.join(to)
-
-    if dtype == "html": # apply GitHub CSS for better style
+    if dtype == "html": # apply CSS for better styling
         content = HTML_HEAD + content + HTML_TAIL
 
     message.attach(MIMEText(content, dtype, ENCODING))
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 from mailsender.__init__ import __version__
 
 setup(
-    name         = "mailsender", 
-    version      = __version__, 
-    description  = "A Python E-mail Sender", 
-    packages     = find_packages(exclude=["test"]), 
+    name        = "mailsender", 
+    version     = __version__, 
+    description = "A Python E-mail Sender", 
+    packages    = find_packages(exclude=["test"]), 
 )
